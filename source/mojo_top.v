0bae9d0c347eb3f5f4206b3334b95d4c33e21ffc
Marcus <marcuspanje@gmail.com>
2015-11-23 18:20:40 -0500

    changed led output to not conflict with signal output

--- source/mojo_top.v
+++ source/mojo_top.v
@@ -6,7 +6,7 @@ module mojo_top(
     // cclk input from AVR, high when AVR is ready
     input cclk,
     // Outputs to the 8 onboard LEDs
-    output[7:0]led,
+    //output[7:0]led,
     // AVR SPI connections
     output spi_miso,
     input spi_ss,
@@ -31,37 +31,6 @@ assign spi_miso = 1'bz;
 assign avr_rx = 1'bz;
 assign spi_channel = 4'bzzzz;
 
-assign led = 8'b1;
-
-/** avr interface
- *  get adc values from pin A0
- */
-wire new_sample;//flag that a new sample is ready
-wire [9:0] sample;
-wire [3:0] sample_channel;
-
-avr_interface avr_interface (
-    .clk(clk),
-    .rst(rst),
-    .cclk(cclk),
-    .spi_miso(spi_miso),
-    .spi_mosi(spi_mosi),
-    .spi_sck(spi_sck),
-    .spi_ss(spi_ss),
-    .spi_channel(spi_channel),
-    .tx(avr_rx),
-    .rx(avr_tx),
-    .channel(4'b0),//sample at pinA0
-    .new_sample(new_sample),
-    .sample(sample),
-    .sample_channel(sample_channel),
-    .tx_data(8'h00),
-    .new_tx_data(1'b0),
-    .tx_busy(),
-    .tx_block(avr_rx_busy),
-    .rx_data(),
-    .new_rx_data()
-  );
 
 /** generate shift registers */
 
